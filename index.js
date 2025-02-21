97044796ecc6a5ade34e9ad96b6a85022aa7e398
Aryan Prince <aryanprincesanty@icloud.com>
2025-02-21 15:30:22 +0300

    Improve logging

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 console.log("Hello world!");
 
+function addNumbers(a, b) {
+  return a + b;
+}
+
 function multiplyNumbers(a, b) {
   return a * b;
 }
 
-console.log(multiplyNumbers(4, 5));
+console.log("4 + 5 = ", addNumbers(4, 5));
+console.log("4 * 5 = ", multiplyNumbers(4, 5));
