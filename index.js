aea53822bf92bd1bdbe2e5ef5cfa4fbd7abf2b15
Aryan Prince <aryanprincesanty@icloud.com>
2025-02-21 15:47:21 +0300

    Add function to divide numbers

--- index.js
+++ index.js
@@ -12,6 +12,11 @@ function subtractNumbers(a, b) {
   return a - b;
 }
 
+function divideNumbers(a, b) {
+  return a / b;
+}
+
 console.log("4 + 5 = ", addNumbers(4, 5));
 console.log("4 * 5 = ", multiplyNumbers(4, 5));
 console.log("9 - 3 = ", subtractNumbers(9, 3));
+console.log("10 / 2 = ", divideNumbers(10, 2));
